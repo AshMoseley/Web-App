287522c2e8a0feec25e66cef4e9552ae6a7eaf1f
ash.moseley <ashman12344@gmail.com>
2023-02-21 02:44:13 +0000

    Created Seeder and Factory for post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Post;
+use App\Models\Comment;
+use Database\Factories\PostFactory;
+use Database\Factories\CommentFactory;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +24,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        Post::factory()
+        ->count(10)
+        ->has(Comment::factory()->count(2))
+        ->create();
     }
 }
---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Post;
+use App\Models\Comment;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Post::factory()
+        ->count(10)
+        ->has(Comment::factory()->count(2))
+        ->create();
+    }
+}

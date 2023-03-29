d1e170999615ffd3f5a812bd76a34fad14976bfd
ash.moseley <ashman12344@gmail.com>
2023-03-29 21:01:41 +0100

    PostController with views and ForumController

---
+++ app/Http/Controllers/ForumController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ForumController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $forums = Forum::all();
+
+        return view('forums.index', compact('forums'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('forums.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'required',
+        ]);
+
+        $forum = new Forum;
+        $forum->name = $validatedData['name'];
+        $forum->description = $validatedData['description'];
+        $forum->save();
+
+        return redirect()->route('forum.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Forum $forum)
+    {
+        return view('forums.show', compact('forum'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Forum $forum)
+    {
+        return view('forums.edit', compact('forum'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Forum $forum)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'required',
+        ]);
+
+        $forum->name = $validatedData['name'];
+        $forum->description = $validatedData['description'];
+        $forum->save();
+
+        return redirect()->route('forum.show', $forum);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Forum $forum)
+    {
+        $forum->delete();
+
+        return redirect()->route('forum.index');
+    }
+}
